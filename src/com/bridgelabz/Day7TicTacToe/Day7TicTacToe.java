91b34f2c3d4c2199a9dd9bb401a83af74cd5655a
ShreyasNandish <shreyasnandish4@gmail.com>
2022-07-14 18:08:31 +0530

    Modified: changed the code to ask for the desired location in box to move

--- src/com/bridgelabz/Day7TicTacToe/Day7TicTacToe.java
+++ src/com/bridgelabz/Day7TicTacToe/Day7TicTacToe.java
@@ -1,8 +1,16 @@
 package com.bridgelabz.Day7TicTacToe;
 
+import java.util.Locale;
 import java.util.Scanner;
 
+
+
 public class Day7TicTacToe {
+    static char computerCharacter;
+    static char userCharacter;
+    static int position;
+    static final int userPlayer=1;
+    static final int computer=2;
 
     static char[] board=new char[10];
     static void createBoard(char[] board)
@@ -25,32 +33,97 @@ public class Day7TicTacToe {
         }
     }
 
-    //public static void updateBoard(int position, int player,board) {
-    public static char chooseLetter() {
-        char character;
+
+    public static void chooseLetter() {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the character you want to choose :");
-        character = sc.next().charAt(0);
-        return character;
+        userCharacter = sc.next().toUpperCase().charAt(0);
+        computerCharacter=(userCharacter=='X')?'O':'X';
+
+        System.out.println("The user selected letter is :"+userCharacter);
+        System.out.println("The computer selected letter is :"+computerCharacter);
     }
 
-    static char computerCharacter() {
-        char character = chooseLetter();
-        if (character == 'x' || character == 'X') {
-            return 'O';
-        } else {
-            return 'X';
+
+    static void playerMove()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the position to which you have to move :");
+        position=sc.nextInt();
+        while(board[position]!='-')
+        {
+            System.out.println("Not a valid movie ");
+            position=sc.nextInt();
         }
+        updateBoard(position,board,userCharacter);
+    }
+    static void updateBoard(int position,char[] board,char userCharacter){
+
+        userCharacter=doToss();
+        switch (position){
+            case 1:
+                board[1]=userCharacter;
+                showBoard(board);
+                break;
+            case 2:
+                board[2]=userCharacter;
+                showBoard(board);
+                break;
+            case 3:
+                board[3]=userCharacter;
+                showBoard(board);
+                break;
+            case 4:
+                board[4]=userCharacter;
+                showBoard(board);
+                break;
+            case 5:
+                board[5]=userCharacter;
+                showBoard(board);
+                break;
+            case 6:
+                board[6]=userCharacter;
+                showBoard(board);
+                break;
+            case 7:
+                board[7]=userCharacter;
+                showBoard(board);
+                break;
+            case 8:
+                board[8]=userCharacter;
+                showBoard(board);
+                break;
+            case 9:
+                board[9]=userCharacter;
+                showBoard(board);
+                break;
+            default:
+                break;
+        }
+
     }
 
-
-
+        static char doToss()
+        {
+            int option = (int) ((Math.random() * 10 % 2) + 1);
+            if(option==userPlayer)
+            {
+                System.out.println("user is playing the chance");
+                return userCharacter;
+            }
+            else
+            {
+                System.out.println("Computer is playing the chance");
+                return  computerCharacter;
+            }
+           // return option;
+        }
 
     public static void main(String[] args) {
         createBoard(board);
-        char symbol=computerCharacter();
-        System.out.println("The computer selected symbol is :"+symbol);
-        showBoard(board);
+        chooseLetter();
+        playerMove();
 
     }
 
